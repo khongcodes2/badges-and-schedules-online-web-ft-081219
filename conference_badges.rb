69a2004b23f4c1e3335044c11317c4e48e1c2dce
K Hong <albtrossfacefall@gmail.com>
2019-08-16 08:58:38 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,4 +1,9 @@
- 
 def badge_maker(name)
   "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  badge_array=[]
+  array.each {|name|badge_array.push(badge_maker(name))}
+  badge_array
 end
